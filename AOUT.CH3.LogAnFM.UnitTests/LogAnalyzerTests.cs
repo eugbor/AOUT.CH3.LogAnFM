93d0a75b3ae6864d4a45a231070e55b4b4c83478
eugeneborodin <eugeneborodin@mail.ru>
2017-02-06 01:00:34 +0300

    init

---
+++ AOUT.CH3.LogAnFM.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AOUT.CH3.LogAnFM.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void overrideTest()
+        {
+            FakeExtensionManager stub = new FakeExtensionManager();
+            stub.WillBeValid = true;
+            TestableLogAnalyzer logan = new TestableLogAnalyzer(stub);
+
+            bool result = logan.IsValidLogFileName("file.ext");
+
+            Assert.True(result);
+        }
+    }
+
+    class TestableLogAnalyzer : LogAnalyzerUsingFactoryMethod
+    {
+        public TestableLogAnalyzer(IExtensionManager mgr)
+        {
+            Manager = mgr;
+        }
+        public IExtensionManager Manager;
+        protected override IExtensionManager GetManager()
+        {
+            return Manager;
+        }
+    }
+    internal class FakeExtensionManager : IExtensionManager
+    {
+        public bool WillBeValid = false;
+        public bool IsValid(string fileName)
+        {
+            return WillBeValid;
+        }
+    }
+}
---
+++ AOUT.CH3.LogAnFM/LogAnalyzerUsingFactoryMethod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOUT.CH3.LogAnFM
+{
+    public interface IExtensionManager
+    {
+        bool IsValid(string fileName);
+    }
+
+    public class FileExtensionManager : IExtensionManager
+    {
+        public bool IsValid(string fileName)
+        {
+            return true;
+            // здесь читается файл                   
+        }
+    }
+
+    public class LogAnalyzerUsingFactoryMethod
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            return GetManager().IsValid(fileName);
+        }
+
+        protected virtual IExtensionManager GetManager()
+        {
+            return new FileExtensionManager();
+        }
+    }
+}
